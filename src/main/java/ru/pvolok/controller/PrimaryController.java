3b8d06114d46ed41ca2b11ae2eb3d143563d18ea
Pavel Volokitin <pavel_volokitin@relex.ru>
2019-11-19 18:10:00 +0300

    Added menubar to application window

--- src/main/java/ru/pvolok/controller/PrimaryController.java
+++ src/main/java/ru/pvolok/controller/PrimaryController.java
@@ -1,4 +1,12 @@
 package ru.pvolok.controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
 public class PrimaryController {
+
+	@FXML
+	private void quitApplication() {
+		Platform.exit();
+	}
 }
