0e4a7ac898efd8fe4e12714437723e9f3df8b2a1
Pavel Volokitin <pavel_volokitin@relex.ru>
2019-11-19 18:33:06 +0300

    Added choose folder button and path label

--- src/main/java/ru/pvolok/controller/PrimaryController.java
+++ src/main/java/ru/pvolok/controller/PrimaryController.java
@@ -2,8 +2,30 @@ package ru.pvolok.controller;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class PrimaryController {
+
+	@FXML
+	private TextField folderPath;
+
+	@FXML
+	private AnchorPane mainPane;
+
+	@FXML
+	private void chooseFolderAction() {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		File dir = directoryChooser.showDialog(mainPane.getScene().getWindow());
+		if (dir != null) {
+			folderPath.setText(dir.getAbsolutePath());
+		} else {
+			folderPath.setText(null);
+		}
+	}
 
 	@FXML
 	private void quitApplication() {
