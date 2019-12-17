1085e7a8bfe9a216385a05df5d07ee4bfb3ecd80
Pavel Volokitin <pvolok@gmail.com>
2019-12-17 16:43:25 +0300

    Created parser for cue file format

--- src/main/java/ru/pvolok/controller/PrimaryController.java
+++ src/main/java/ru/pvolok/controller/PrimaryController.java
@@ -5,8 +5,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
+import ru.pvolok.helper.cue.CueObject;
+import ru.pvolok.utils.CueUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PrimaryController {
 
@@ -17,11 +26,17 @@ public class PrimaryController {
 	private AnchorPane mainPane;
 
 	@FXML
-	private void chooseFolderAction() {
+	private void chooseFolderAction() throws IOException {
 		DirectoryChooser directoryChooser = new DirectoryChooser();
 		File dir = directoryChooser.showDialog(mainPane.getScene().getWindow());
 		if (dir != null) {
-			folderPath.setText(dir.getAbsolutePath());
+			String dirPath = dir.getAbsolutePath();
+			List<String> cueFiles = Files.walk(Paths.get(dirPath))
+					.map(x -> x.toString())
+					.filter(f -> f.endsWith(".cue"))
+					.collect(Collectors.toList());
+			CueObject cueSheet = CueUtils.readFromFile(new File(cueFiles.get(0)));
+			folderPath.setText(dirPath);
 		} else {
 			folderPath.setText(null);
 		}
--- src/main/java/ru/pvolok/utils/CueUtils.java
+++ src/main/java/ru/pvolok/utils/CueUtils.java
@@ -1,17 +1,24 @@
 package ru.pvolok.utils;
 
 import ru.pvolok.helper.cue.CueObject;
+import ru.pvolok.helper.cue.CueTime;
+import ru.pvolok.helper.cue.CueTrack;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CueUtils {
 
 	public static CueObject readFromFile(File file) throws IOException {
 		CueObject cueObject = new CueObject();
+		List<CueTrack> tracks = new ArrayList<>();
 		Files.lines(file.toPath()).forEach(line -> {
 			String key, value;
+			CueTrack currentTrack = new CueTrack();
+			line = line.strip();
 			switch (line.split("\\s")[0]) {
 				case "REM":
 					key = line.split("\\s")[1];
@@ -21,17 +28,50 @@ public class CueUtils {
 				case "PERFORMER":
 					key = line.split("\\s")[0];
 					value = line.split(key)[1].strip();
-					cueObject.setPerformer(StringUtils.removeQuotes(value));
+					String performer = StringUtils.removeQuotes(value);
+					if (!tracks.isEmpty()) {
+						tracks.get(tracks.size() - 1).setPerformer(performer);
+					} else {
+						cueObject.setPerformer(performer);
+					}
 					break;
 				case "TITLE":
 					key = line.split("\\s")[0];
 					value = line.split(key)[1].strip();
-					cueObject.setTitle(StringUtils.removeQuotes(value));
+					String title = StringUtils.removeQuotes(value);
+					if (!tracks.isEmpty()) {
+						tracks.get(tracks.size() - 1).setTitle(title);
+					} else {
+						cueObject.setTitle(title);
+					}
 					break;
 				case "FILE":
 					cueObject.setFile(line.split("\"")[1]);
+					break;
+				case "INDEX":
+					String[] timeArray = line.split("\\s")[2].split(":");
+					String indexNumber = line.split("\\s")[1];
+					int minute = Integer.parseInt(timeArray[0]);
+					int second = Integer.parseInt(timeArray[1]);
+					int frame = Integer.parseInt(timeArray[2]);
+					CueTime index = new CueTime(minute, second, frame);
+					if ("00".equals(indexNumber)) {
+						tracks.get(tracks.size() - 1).setIndex00(index);
+					} else if ("01".equals(indexNumber)) {
+						tracks.get(tracks.size() - 1).setIndex01(index);
+					}
+					break;
+				case "TRACK":
+					int number = Integer.parseInt(line.split("\\s")[1]);
+					currentTrack.setNumber(number);
+					String type = line.split("\\s")[2];
+					type = type.substring(0, 1).toUpperCase() + type.substring(1).toLowerCase();
+					currentTrack.setType(type);
+					tracks.add(currentTrack);
+					break;
 			}
 		});
-		return null;
+		cueObject.setTracks(tracks);
+		return cueObject;
 	}
 }
